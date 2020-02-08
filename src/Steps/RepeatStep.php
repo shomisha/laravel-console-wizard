81a255c6aa4de766a0aac479da36c1371c6d1822
Misa Kovic <misa95kovic@gmail.com>
2020-02-08 01:10:45 +0100

    feat(wizard): implement the ability to repeat a step

---
+++ src/Steps/RepeatStep.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Shomisha\LaravelConsoleWizard\Steps;
+
+use Shomisha\LaravelConsoleWizard\Command\Wizard;
+use Shomisha\LaravelConsoleWizard\Contracts\Step;
+use Shomisha\LaravelConsoleWizard\Exception\InvalidStepException;
+
+class RepeatStep implements Step
+{
+    /** @var int */
+    private $counter = 0;
+
+    /** @var callable */
+    private $callback = null;
+
+    /** @var \Shomisha\LaravelConsoleWizard\Contracts\Step */
+    private $step;
+
+    public function __construct(Step $step)
+    {
+        $this->step = $step;
+    }
+
+    public function take(Wizard $wizard)
+    {
+        if ($this->callback === null) {
+            throw new InvalidStepException(
+                "The RepeatStep has not been properly initialized. Please call either RepeatStep::times() or RepeatStep::until() to initialize it."
+            );
+        }
+
+        $answers = [];
+        $answer = null;
+
+        while (call_user_func($this->callback, $answer)) {
+            $answer = $this->step->take($wizard);
+
+            $answers[] = $answer;
+
+            $this->counter++;
+        }
+
+        return $answers;
+    }
+
+    public function times(int $times)
+    {
+        return $this->until(function () use ($times) {
+            return $this->counter < $times;
+        });
+    }
+
+    public function untilAnswerIs($answer)
+    {
+        return $this->until(function ($actualAnswer) use ($answer) {
+            return $actualAnswer !== $answer;
+        });
+    }
+
+    public function until(callable $callback)
+    {
+        $this->callback = $callback;
+
+        return $this;
+    }
+}
