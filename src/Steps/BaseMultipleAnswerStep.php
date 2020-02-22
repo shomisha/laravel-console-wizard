0d06cfa511a0cf92da6fb8581e8b3dd572cdbed2
Misa Kovic <misa95kovic@gmail.com>
2020-02-22 01:57:01 +0100

    feat(multiple-answer-steps): implement more methods on the BaseMultipleAnswer step including repetition tracking and max repetition limit

--- src/Steps/BaseMultipleAnswerStep.php
+++ src/Steps/BaseMultipleAnswerStep.php
@@ -10,11 +10,55 @@ abstract class BaseMultipleAnswerStep extends BaseStep
     /** @var bool */
     protected $retainEndKeywordInAnswers;
 
+    /** @var int  */
+    protected $repetitions = 0;
+
+    /** @var int */
+    protected $maxRepetitions;
+
     public function __construct(string $text, array $options = [])
     {
         parent::__construct($text);
 
         $this->endKeyword = $options['end_keyword'] ?? 'Done';
         $this->retainEndKeywordInAnswers = $options['retain_end_keyword'] ?? false;
+        $this->maxRepetitions = $options['max_repetitions'] ?? null;
+    }
+
+    protected function loop(callable $callback)
+    {
+        $answers = [];
+
+        do {
+            $newAnswer = $callback();
+
+            $answers[] = $newAnswer;
+
+            $this->incrementRepetitions();
+        } while ($this->shouldKeepLooping($newAnswer));
+
+        return $answers;
+    }
+
+    protected function shouldKeepLooping($answer)
+    {
+        return strtolower($answer) !== strtolower($this->endKeyword) && !$this->hasExceededMaxRepetitions();
+    }
+
+    protected function incrementRepetitions()
+    {
+        $this->repetitions++;
+
+        return $this;
+    }
+
+    protected function hasExceededMaxRepetitions()
+    {
+        return $this->maxRepetitions !== null && $this->repetitions >= $this->maxRepetitions;
+    }
+
+    protected function shouldRemoveEndKeyword(array $answers)
+    {
+        return !$this->retainEndKeywordInAnswers && strtolower(last($answers)) === strtolower($this->endKeyword);
     }
 }
